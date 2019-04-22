887f8dc3edcf32e22df205b09d71f3402a778b92
bobo <bobo@bobo.com>
2019-04-22 17:13:28 +0800

    Signed-off-by: bobo <bobo@bobo.com>

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
 function ExpressPermission() {
 	var that = this
-	this.method = 'some'
-	this.getPermissions = function(cb) {
-		console.log('getPermissions')
+	this.reqMethodName = 'somePermission'
+	this.getPermissions = function(req, res, cb) {
 		cb([])
 	}
-	this.some = function(req, res, next) {
-		req[that.method] = function(arg) {
-			that.getPermissions((permissions) => {
-				console.log(permissions)
-				if (typeof arg === 'string') {
-					var re = arg.replace(/(\w)/g,"p='$1'")
-					console.log(re)
-					permissions.some((p) => { return eval(arg)}) ? next() : that.forbidden(req, res)
+	this.some = function(arg, permissions) {
+		if (typeof arg === 'string') {
+			arg = arg.replace(/\(/g,'( ')
+			arg = arg.replace(/\)/g,' )')
+			var str = arg.split(/\s+/).map((x) => {
+				if (['||', '&&', '(', ')'].indexOf(x) > -1) {
+					return x
 				} else {
-					console.log('function')
-					permissions.some(arg) ? next() : that.forbidden(req, res)
+					return permissions.indexOf(x) > -1
 				}
+			}).join(' ')
+			return eval(str)
+		} else {
+			return false
+		}
+	}
+	this.somePermission = function(req, res, next) {
+		req[that.reqMethodName] = function(arg, cb) {
+			that.getPermissions(req, res, (permissions) => {
+				cb(that.some(arg, permissions))
 			})
 		}
 		next()
 	}
+	this.verify = function(arg, forbidden_fn) {
+		return ((arg) => {
+			return function(req, res, next ) {
+				that.getPermissions(req, res, (permissions) => {
+					forbidden_fn = forbidden_fn || that.forbidden
+					that.some(arg, permissions) ? next() : forbidden_fn(req, res, next)
+				})
+			}
+		})(arg, forbidden_fn)
+	}
 	this.forbidden = function(req, res, next) {
 		res.status(403).send('forbidden')
+	}
+	this.next = function(req, res, next) {
+		next()
+	}
+	this.init = function(opt) {
+		opt = opt || {}
+		this.reqMethodName = opt.reqMethodName === undefined ? this.reqMethodName : opt.reqMethodName
+		this.getPermissions = opt.getPermissions === undefined ? this.getPermissions : opt.getPermissions
+		this.forbidden = opt.forbidden === undefined ? this.forbidden : opt.forbidden
+		return this.somePermission
 	}
 }
 
